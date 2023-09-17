1a9ce8d1abeebc18ca0e0fdd6b1fc3e43b8d7451
Abhishek singh chauhan <avi7000201383@gmail.com>
2023-09-17 23:16:04 +0530

    added main section

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import ReactDOM from "react-dom/client";
 import "/index.css";
 import Header from "./Header";
 import Hero from "./hero/Hero";
+import Main from "./main/Main";
 
 const App = () => {
   return (
     <div className="App">
       <Header />
       <Hero />
+      <Main />
     </div>
   );
 };
---
+++ src/main/Main.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./main.css";
+const Main = () => {
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>Order Food from best place near you</h1>
+        <p>
+          Bite is a cutting-edge restaurant app designed to enhance your dining
+          experience. Whether you're a food enthusiast looking for new culinary
+          adventures or just want a quick and convenient way to satisfy your
+          cravings, Foodie Delight has you covered.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
