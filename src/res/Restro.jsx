14bd6cc459fb69977d5c3ae5489559a64e59f19e
Abhishek singh chauhan <avi7000201383@gmail.com>
2023-09-21 10:40:50 +0530

    added carousel - Static now

--- src/res/Restro.jsx
+++ src/res/Restro.jsx
@@ -1,10 +1,19 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./restro.css";
+// import Carousel from "../carouselcomponent/Carousel";
+import { Carousel } from "3d-react-carousal";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 const Restro = () => {
+  let slides = [
+    <img src="https://picsum.photos/800/300/?random" alt="1" />,
+    <img src="https://picsum.photos/800/301/?random" alt="2" />,
+    <img src="https://picsum.photos/800/302/?random" alt="3" />,
+    <img src="https://picsum.photos/800/303/?random" alt="4" />,
+    <img src="https://picsum.photos/800/304/?random" alt="5" />,
+  ];
   useEffect(() => {
     const apiUrl =
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
@@ -26,6 +35,9 @@ const Restro = () => {
           <FontAwesomeIcon icon={faSearch} className="search-icon" />
           <input className="input" placeholder="Search restaurants" />
         </div>
+      </div>
+      <div className="carousel">
+        <Carousel slides={slides} autoplay={true} interval={3000} />
       </div>
     </div>
   );
