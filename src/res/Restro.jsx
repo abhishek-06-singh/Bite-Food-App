d3db8c609b81e8b1789f3ef1600fec29a7832693
Abhishek singh chauhan <avi7000201383@gmail.com>
2023-10-06 01:05:07 +0530

    added server

--- src/res/Restro.jsx
+++ src/res/Restro.jsx
@@ -33,9 +33,7 @@ const Restro = () => {
     setIsLoading(true);
 
     try {
-      const data = await fetch(
-        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-      );
+      const data = await fetch("http://localhost:3000/api/data");
 
       if (!data.ok) {
         throw new Error("Network response was not ok");
